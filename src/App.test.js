8fce8f643deb79e2801f07949f8db8fff1f90314
Patymag29 <patymag29@gmail.com>
2026-03-22 19:14:42 -0600

    initial commit + local test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders my name on the page', () => {//test case to check if the name is rendered on the page
+  render(<App />); //render the App component
+  const nameElement = screen.getByText(/Patricia Diniz Magalhaes/i); //get the element that contains the name using a regular expression to ignore case
+  expect(nameElement).toBeInTheDocument(); //assert that the element is in the document
 });
