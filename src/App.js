92b3dd9cf61b3cbc20999bbaa2a4810868d7ad88
Patymag29 <patymag29@gmail.com>
2026-03-26 19:01:31 -0600

    Added Course name and test of autommatic deploy 3

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Patricia Diniz Magalhaes</h1>
-        <h2>Course: TECH2102 - Enterprise - Winter 2026 </h2>
+        <h2>Course: TECH2102 - Enterprise - Winter 2026</h2>
       </header>
     </div>
   );
