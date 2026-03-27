2b007236fe70d48bd95e160ed6d9e0a6ce5df7a7
Patymag29 <patymag29@gmail.com>
2026-03-26 19:07:13 -0600

    Added Course name and test of autommatic deploy 4

--- src/App.test.js
+++ src/App.test.js
@@ -6,6 +6,6 @@ test('renders my name on the page', () => {//test case to check if the name is r
   const nameElement = screen.getByText(/Patricia Diniz Magalhaes/i); //get the element that contains the name using a regular expression to ignore case
   expect(nameElement).toBeInTheDocument(); //assert that the element is in the document
   
-  const nameElement2 = screen.getByText(/Course: TECH2102 - Enterprise - Winter 2026/i); //get the element that contains the course name using a regular expression to ignore case
+  const nameElement2 = screen.getByText(/Course: TECH2102 - Enterprise - Winter 2026 test/i); //get the element that contains the course name using a regular expression to ignore case
   expect(nameElement2).toBeInTheDocument(); //assert that the element is in the document
 });
