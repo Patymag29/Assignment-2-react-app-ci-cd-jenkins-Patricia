b4bfa2ed9b8dd98c86499dbd92f9de4a02c0847c
Patymag29 <patymag29@gmail.com>
2026-03-27 20:55:38 -0600

    word test removed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 
+import './App.css';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Patricia Diniz Magalhaes</h1>
-        <h2>Course: TECH2102 - Enterprise - Winter 2026 </h2>
+        <h2>Course: TECH2102 - Enterprise - Winter 2026</h2> 
       </header>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders my name on the page', () => {//test case to check if the name is rendered on the page
-  render(<App />); //render the App component
-  const nameElement = screen.getByText(/Patricia Diniz Magalhaes/i); //get the element that contains the name using a regular expression to ignore case
-  expect(nameElement).toBeInTheDocument(); //assert that the element is in the document
+test('renders my name on the page', () => {
+  render(<App />);
   
-  const nameElement2 = screen.getByText(/Course: TECH2102 - Enterprise - Winter 2026 /i); //get the element that contains the course name using a regular expression to ignore case
-  expect(nameElement2).toBeInTheDocument(); //assert that the element is in the document
+  const nameElement = screen.getByText(/Patricia Diniz Magalhaes/i);
+  expect(nameElement).toBeInTheDocument();
+  
+  // Remova o espaço extra no final da expressão regular
+  const courseElement = screen.getByText(/Course: TECH2102 - Enterprise - Winter 2026/i);
+  expect(courseElement).toBeInTheDocument();
 });
